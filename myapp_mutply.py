3147c28e4806a97f37142e1d671ebaefdbc59160
Arthurmlopez <89947351+Arthurmlopez@users.noreply.github.com>
2021-09-02 17:42:12 -0300

    Update myapp_mutply.py

--- myapp_mutply.py
+++ myapp_mutply.py
@@ -1,2 +1,13 @@
-def soma(n1,n2):
-  pass
+def soma(num1,num2):
+  if is_number(num1) and is_number(num2):
+   return float(num1) * float(num2)
+  else:
+   return None
+  
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
