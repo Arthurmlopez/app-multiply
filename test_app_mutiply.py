fe416c60762d6406aa62bc394fa5524cbcdf7916
Arthurmlopez <89947351+Arthurmlopez@users.noreply.github.com>
2021-09-02 17:46:09 -0300

    Update test_app_mutiply.py

--- test_app_mutiply.py
+++ test_app_mutiply.py
@@ -1,6 +1,6 @@
 import pytest
 from myapp_mutply import mutiply
 
-@pytest.mark.parametrize('n1,n2,expected', [(0, 0, 0), (0,1,0), (2,1,2), (7,3,21), (2.3,5,11.5), ("dois",0,None), (-3,2,-6)])
+@pytest.mark.parametrize('num1,num2,expected', [(0, 0, 0), (0,1,0), (2,1,2), (7,3,21), (2.3,5,11.5), ("dois",0,None), (-3,2,-6)])
 def test_soma(num1,num2, expected):
     assert soma(num1,num2) == expected
